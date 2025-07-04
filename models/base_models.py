261f5d70b6447b02b0222e03b996d4e07864570c
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-04 22:34:37 +0530

    Implemented Bidirectional LSTM with GloVe

--- models/base_models.py
+++ models/base_models.py
@@ -60,7 +60,7 @@ class BidirectionalRNN(nn.Module):
 
         embedded = self.embedding(input_ids)
         packed = nn.utils.rnn.pack_padded_sequence(embedded , lengths.cpu(), batch_first=True,enforce_sorted=False)
-        packed_output , hidden = self.rnn(packed)
+        packed_output , hidden = self.bidirectionalrnn(packed)
         hidden_cat = torch.cat((hidden[0] , hidden[1]) , dim=1)
         output = self.fc(self.dropout(hidden_cat))
 
@@ -68,4 +68,26 @@ class BidirectionalRNN(nn.Module):
     
 
 
+
+class BidirectionalLSTM(nn.Module):
+    def __init__(self,embedding_matrix,hidden_dim,output_dim,padding_idx=0,dropout=0.3,freeze_embeddings=False):
+        super(BidirectionalLSTM,self).__init__()
+        vocab_size , embedding_dim = embedding_matrix.shape
+
+        self.embedding = nn.Embedding.from_pretrained(embedding_matrix,freeze=freeze_embeddings,padding_idx = padding_idx)
+        self.bidirectionallstm = nn.LSTM(embedding_dim,hidden_dim,batch_first=True,bidirectional=True)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim*2 , output_dim)
+
+
+    def forward(self,input_ids,lengths):
+
+        embedded = self.embedding(input_ids)
+        packed_embedding = nn.utils.rnn.pack_padded_sequence(embedded , lengths.cpu(), batch_first=True,enforce_sorted=False)
+        packed_output , (hidden , cell) = self.bidirectionallstm(packed_embedding)
+        hidden_cat = torch.cat((hidden[0],hidden[1]),dim=1)
+        output = self.fc(self.dropout(hidden_cat))
+
+        return output
+    
 
