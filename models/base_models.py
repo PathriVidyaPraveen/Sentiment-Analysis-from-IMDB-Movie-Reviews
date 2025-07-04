1ce0a8265445689762a7cadba4c6d122ab6131a4
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-04 22:22:07 +0530

    Implemented Vanilla LSTM and Bidirectional RNN with GloVe

--- models/base_models.py
+++ models/base_models.py
@@ -20,3 +20,52 @@ class VanillaRNN(nn.Module):
         output = self.fc(self.dropout(last_hidden))
         return output
     
+
+
+class VanillaLSTM(nn.Module):
+    def __init__(self,embedding_matrix,hidden_dim,output_dim,padding_idx=0,dropout=0.3,freeze_embeddings=False):
+        super(VanillaLSTM,self).__init__()
+        vocab_size , embedding_dim = embedding_matrix.shape
+
+        self.embedding = nn.Embedding.from_pretrained(embedding_matrix,freeze= freeze_embeddings,padding_idx = padding_idx)
+        self.lstm = nn.LSTM(embedding_dim , hidden_dim ,batch_first = True)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim , output_dim)
+
+    def forward(self,input_ids , lengths):
+
+        embedded = self.embedding(input_ids)
+        packed_embeddings = nn.utils.rnn.pack_padded_sequence(embedded , lengths.cpu() , batch_first=True,enforce_sorted = False)
+        packed_output , (hidden , _) = self.lstm(packed_embeddings)
+        last_hidden = hidden.squeeze(0)
+        output = self.fc(self.dropout(last_hidden))
+
+        return output
+    
+
+
+class BidirectionalRNN(nn.Module):
+    def __init__(self,embedding_matrix, hidden_dim , output_dim , padding_idx = 0 , dropout = 0.3 , freeze_embeddings=False):
+        super(BidirectionalRNN, self).__init__()
+        vocab_size , embedding_dim = embedding_matrix.shape
+
+        self.embedding = nn.Embedding.from_pretrained(embedding_matrix , freeze = freeze_embeddings , padding_idx = padding_idx)
+        self.bidirectionalrnn = nn.rnn(embedding_dim , hidden_dim, batch_first=True, bidirectional=True)
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim*2 , output_dim)
+
+    
+
+    def forward(self,input_ids,lengths):
+
+        embedded = self.embedding(input_ids)
+        packed = nn.utils.rnn.pack_padded_sequence(embedded , lengths.cpu(), batch_first=True,enforce_sorted=False)
+        packed_output , hidden = self.rnn(packed)
+        hidden_cat = torch.cat((hidden[0] , hidden[1]) , dim=1)
+        output = self.fc(self.dropout(hidden_cat))
+
+        return output
+    
+
+
+
